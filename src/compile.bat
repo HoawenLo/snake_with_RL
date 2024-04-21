71d2bc2f6286ef0f5b9bf5c32430b0200b0757fa
HoawenLo <hoawenlo@yahoo.com>
2024-04-21 20:27:30 +0100

    Managed to finally get main.cpp to compile with raylib.

---
+++ src/compile.bat
@@ -0,0 +1,1 @@
+ g++ main.cpp -o test -lraylib -Iinclude/ -Lbin/ -lraylib -lopengl32 -lgdi32 -lwinmm
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
- 
+#include <iostream>
+#include "include/raylib.h"
+
+using namespace std;
+
+int main() {
+	cout << "Starting new game..." << endl;
+	InitWindow(750, 750, "Retro Snake");
+
+	CloseWindow();
+	return 0;
+}
